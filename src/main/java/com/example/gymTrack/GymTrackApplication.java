ab5b04ae3ef7bc35d41657ad479f85f0807904a7
AleksanderBanasiak <olek.banasiak0@gmail.com>
2024-05-25 18:58:12 +0200

    new project

---
+++ src/main/java/com/example/gymTrack/GymTrackApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gymTrack/GymTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gymTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
